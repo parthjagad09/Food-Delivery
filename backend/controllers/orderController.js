cea7a0c9f4e8a51e0018b991c1c063049c829e37
parthjagad09 <pvj911911@gmail.com>
2026-01-28 23:09:15 +0530

    Verification of Payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,8 +38,8 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            success_url: `https://food-delivery-parth.vercel.app/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `https://food-delivery-parth.vercel.app/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `https://food-delivery-7phs.vercel.app/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `https://food-delivery-7phs.vercel.app/verify?success=false&orderId=${newOrder._id}`,
         });
 
         res.json({ success: true, session_url: session.url });
